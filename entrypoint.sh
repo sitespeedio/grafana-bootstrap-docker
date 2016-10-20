33d6d809cf8f9610bb46ba1d6eb837fb24577be6
soulgalore <peter@soulgalore.com>
2016-10-20 10:50:45 +0200

    hello world

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+GF_API=${GF_API:-http://grafana:3000/api}
+
+echo "Waiting for Grafana API..."
+
+curl -u sitespeedio:172ga59Kuab51mL ${GF_API}/datasources -f &> /dev/null
+while [ $? -ne 0 ]; do
+  sleep 2
+  curl -u $GF_USER:$GF_PASSWORD ${GF_API}/datasources -f &> /dev/null
+done
+
+echo "Up!"
+
+echo "Adding datasources..."
+
+for datasource in `ls -1 /datasources/*.json`; do
+  echo "Adding ${datasource}"
+  curl -s -H 'Content-Type:application/json' -u $GF_USER:$GF_PASSWORD --data @"${datasource}" ${GF_API}/datasources
+  echo ""
+done
+
+echo "Adding dashboards..."
+
+for dashboard in `ls -1 /dashboards/*.json`; do
+  echo "Adding ${dashboard}"
+  curl -s -H 'Content-Type:application/json' -u $GF_USER:$GF_PASSWORD --data @"${dashboard}" ${GF_API}/dashboards/db
+  echo ""
+done
+
+echo "Done!"
